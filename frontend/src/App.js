58643bdda326bf07a195051ed830fb2d91de0a64
mlaruns <mlarunkumar@gmail.com>
2025-08-07 17:06:56 +0530

    Add basic React App.js component

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>AI Project Planner</h1>
+      <p>Welcome to your project planner!</p>
+    </div>
+  );
+}
+
+export default App;
